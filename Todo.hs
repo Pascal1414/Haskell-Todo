f571549f6b99c1f2af23d49311e583cc04d04587
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-14 13:34:53 +0100

    Update Todo.hs
    Add IORef / Add Add-Function / Update Remove Function

--- Todo.hs
+++ Todo.hs
@@ -2,27 +2,27 @@ import System.Console.Haskeline (getInputLine)
 import Text.Read (readMaybe)
 import System.Exit (exitSuccess)
 import Control.Monad.RWS (MonadState(put))
+import Data.IORef
+import Data.List(delete)
 
-data Todo = Todo { title :: String, doUntil :: String }
-
-todos :: [Todo]
-todos = [Todo  "Buy milk" "2020-12-24", Todo  "Learn Haskell" "2020-12-25"]
+data Todo = Todo { title :: String, doUntil :: String } deriving (Eq)
 
 main :: IO()
 main = do
+    ref <- newIORef []
     putStrLn "Welcome to Todo-App"
-    menu
+    menu ref
 
-data MenuOption = MenuOption { name :: String, function :: IO () }
+data MenuOption = MenuOption { name :: String, function ::IORef [Todo] ->  IO () }
 menuOptions :: [MenuOption]
 menuOptions = [MenuOption "Add a new todo" addTodo, MenuOption "Show all todos" showTodos, MenuOption "Remove a todo" removeTodo, MenuOption "Exit" exit]
 
-menu :: IO()
-menu = do
+menu :: IORef [Todo] -> IO() 
+menu ref  = do
     printMenu
     menuSelection <-  getMenuSelection
-    function (menuOptions !! (menuSelection - 1))
-    menu
+    function (menuOptions !! (menuSelection - 1)) ref
+    menu ref
 
 printMenu :: IO ()
 printMenu = do
@@ -48,20 +48,28 @@ getNumberInput = do
             putStrLn "Invalid input. Please enter a valid number."
             getNumberInput
 
-getTodoSelection :: IO Todo
-getTodoSelection = do
+getTodoSelection :: [Todo] -> IO Int
+getTodoSelection todos = do
     number <- getNumberInput
     if number > length todos || number < 1
     then do
         putStrLn "Todo does not exist"
-        getTodoSelection
-    else return (todos !! (number - 1))
+        getTodoSelection todos
+    else return (number - 1)
 
-addTodo :: IO ()
-addTodo = putStrLn "Adding a new task"
+addTodo :: IORef [Todo] -> IO ()
+addTodo ref = do
+    todos <- readIORef ref
+    putStrLn "Title:"
+    title <- getLine
+    putStrLn "DoUntil"
+    doUntil <- getLine
+    let newTodo = Todo {title = title, doUntil = doUntil}
+    writeIORef ref (newTodo : todos)
 
-showTodos :: IO ()
-showTodos = do
+showTodos :: IORef [Todo] -> IO ()
+showTodos ref = do
+    todos <- readIORef ref
     if length todos > 0
     then do
         putStrLn "-----------------"
@@ -70,14 +78,19 @@ showTodos = do
         putStrLn "-----------------"
     else putStrLn "No todos yet"
 
-removeTodo :: IO ()
-removeTodo = do
-    showTodos
-    putStrLn "Which todo do you want to remove?"
-    removeTodo <- getTodoSelection
-    putStrLn "test"
+removeTodo :: IORef [Todo] -> IO ()
+removeTodo ref = do   
+    todos <- readIORef ref
+    if not (null todos) && length todos /= 0  
+    then do
+        showTodos ref
+        putStrLn "Which todo do you want to remove?"  
+        removeTodoIndex <- getTodoSelection todos
+        writeIORef ref (delete (todos !! removeTodoIndex) todos)
+        putStrLn ""
+    else putStrLn "There are no todos"
 
-exit :: IO ()
-exit = do
+exit :: IORef [Todo] -> IO ()
+exit ref = do
     putStrLn "Ok Bye Bye!"
     exitSuccess
