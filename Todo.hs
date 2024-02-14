099a5b2cc356ca471cdf50baff5515aea69c7506
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-14 10:16:48 +0100

    Update Todo.hs
    Tasks have now functions that get executed

--- Todo.hs
+++ Todo.hs
@@ -1,26 +1,28 @@
 import System.Console.Haskeline (getInputLine)
 import Text.Read (readMaybe)
+import System.Win32 (xBUTTON1)
 
 data Todo = Todo { id :: Int, title :: String, doUntil :: String }
 
 main :: IO()
 main = do
-    putStrLn "Welcome to Todo-App"   
+    putStrLn "Welcome to Todo-App"
     menu
 
-
-menuOptions :: [String]
-menuOptions = ["Print Todos", "Add Todo", "Remove Todo"]
+data MenuOption = MenuOption { name :: String, function :: IO () }
+menuOptions :: [MenuOption]
+menuOptions = [MenuOption "Add a new task" addTask, MenuOption "Show all tasks" showTasks, MenuOption "Remove a task" removeTask, MenuOption "Exit" exit]
 
 menu :: IO()
 menu = do
     printMenu
-    getMenuSelection
+    menuSelection <-  getMenuSelection
+    function (menuOptions !! (menuSelection - 1))
     menu
 
 printMenu :: IO ()
 printMenu = do
-    mapM_ putStrLn $ zipWith (\i opt -> show i ++ ". " ++ opt) [1..] menuOptions
+    mapM_ putStrLn $ zipWith (\i opt -> show i ++ ". " ++ opt) [1..] (map name menuOptions)
 
 getMenuSelection :: IO Int
 getMenuSelection = do
@@ -30,9 +32,8 @@ getMenuSelection = do
         putStrLn "Option does not exist"
         getMenuSelection
     else return number
-    
-    
-    
+
+
 getNumberInput :: IO Int
 getNumberInput = do
     putStrLn "Please enter a number:"
@@ -43,3 +44,15 @@ getNumberInput = do
             putStrLn "Invalid input. Please enter a valid number."
             getNumberInput
 
+
+addTask :: IO ()
+addTask = putStrLn "Adding a new task"
+
+showTasks :: IO ()
+showTasks = putStrLn "Showing all tasks"
+
+removeTask :: IO ()
+removeTask = putStrLn "Removing a task"
+
+exit :: IO ()
+exit = putStrLn "Exiting the program"
