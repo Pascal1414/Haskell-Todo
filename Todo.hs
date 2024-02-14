1e12a1e6c172e881ab52a86a82755b54ccf87cc6
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-14 09:54:32 +0100

    Update Todo.hs
    Add Print Menu

--- Todo.hs
+++ Todo.hs
@@ -1,19 +1,45 @@
+import System.Console.Haskeline (getInputLine)
+import Text.Read (readMaybe)
+
 data Todo = Todo { id :: Int, title :: String, doUntil :: String }
 
 main :: IO()
 main = do
-    putStrLn "Welcome to Todo-App"
-    getMenuSelection
+    putStrLn "Welcome to Todo-App"   
+    menu
 
 
 menuOptions :: [String]
-menuOptions = [
-    "Print Todos",
-    "Add Todo",
-    "Remove Todo"
-]
+menuOptions = ["Print Todos", "Add Todo", "Remove Todo"]
 
-getMenuSelection :: IO ()
+menu :: IO()
+menu = do
+    printMenu
+    getMenuSelection
+    menu
+
+printMenu :: IO ()
+printMenu = do
+    mapM_ putStrLn $ zipWith (\i opt -> show i ++ ". " ++ opt) [1..] menuOptions
+
+getMenuSelection :: IO Int
 getMenuSelection = do
-    putStrLn "What do you want to do?"
+    number <- getNumberInput
+    if number > length menuOptions || number < 1
+    then do
+        putStrLn "Option does not exist"
+        getMenuSelection
+    else return number
     
+    
+    
+getNumberInput :: IO Int
+getNumberInput = do
+    putStrLn "Please enter a number:"
+    input <- getLine
+    case readMaybe input of
+        Just number -> return number
+        Nothing -> do
+            putStrLn "Invalid input. Please enter a valid number."
+            getNumberInput
+
